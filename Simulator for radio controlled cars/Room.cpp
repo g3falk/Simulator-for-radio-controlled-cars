190a48d7dfc4a1734410d0a0f6680a029178d5f9
g3falk <g3falk@gmail.com>
2025-03-24 22:54:48 +0100

    fixed a error in parsing commands, fixed so cars can not be "inside" walls, update the readme

--- Simulator for radio controlled cars/Room.cpp
+++ Simulator for radio controlled cars/Room.cpp
@@ -10,7 +10,7 @@ Room::Room(int inputwidth, int inputheight){
 
 bool Room::isPositionValid(int x, int y){
 
-    if (x >= 0 && x <= width && y >= 0 && y <= height) {
+    if (x >= 0 && x < width && y >= 0 && y < height) {
         return true;
     }        
     else {
--- Simulator for radio controlled cars/Simulator.cpp
+++ Simulator for radio controlled cars/Simulator.cpp
@@ -1,5 +1,7 @@
 #include "Simulator.h"
 
+using namespace std;
+
 Simulator::Simulator(){
     commands = "";
 }
@@ -7,49 +9,46 @@ Simulator::Simulator(){
 void Simulator::run() {
     try{
         //Setup Room where height and width must be postive integers
-        std::cout << "Enter Room Height and Width: ";
+        cout << "Enter Room Height and Width: ";
         int width, length;
-        std::cin >> width >> length;
+        cin >> width >> length;
         Room room(width, length);
 
         //Setup Car where the car postions needs to be inside the room and a valid direction
-        std::cout << "Enter starting Position (X Y) and Direction (N/S/E/W): ";
+        cout << "Enter starting Position (X Y) and Direction (N/S/E/W): ";
         int startX, startY;
         char direction;
-        std::cin >> startX >> startY >> direction;
+        cin >> startX >> startY >> direction;
         if (!room.isPositionValid(startX, startY)) {
-            throw std::invalid_argument("Car starts outside the room.");
+            throw invalid_argument("Car starts outside the room.");
         }
         Car car(startX, startY, direction);
 
         //take users commands and runs coresponding function call, the commands must be f/b/l/r
-        std::cout << "Enter starting commands: ";
-        std::cin >> commands;        
+        cout << "Enter starting commands: ";
+        cin >> commands;        
         for (char cmd : commands) {
-            switch (cmd) {
+            switch (tolower(cmd)) {
             case 'f': car.moveForward(); break;
             case 'b': car.moveBackward(); break;
             case 'l': car.rotateLeft(); break;
             case 'r': car.rotateRight(); break;
             default:
-                throw std::invalid_argument("Invalid command. Use F/B/L/R.");
+                throw invalid_argument("Invalid command. Use F/B/L/R.");
             }
 
             //checking if the car hit a wall
             if (!room.isPositionValid(car.getX(), car.getY())) {
-                std::cout << "Unsuccessful: Car hit the wall at (" << car.getX() << ", " << car.getY() << ").\n";
+                cout << "Unsuccessful: Car hit the wall at (" << car.getX() << ", " << car.getY() << ").\n";
                 return;
             }
         }
 
         //The car did not crash and prints out the position of the car and its direction
-        std::cout << "Successful: Final position ("
-            << car.getX() << ", "
-            << car.getY() << ") "
-            << car.getDirection() << "\n";
+        cout << "Successful: Final position (" << car.getX() << ", " << car.getY() << ") " << car.getDirection() << "\n";
 
     }
-    catch (const std::exception& e) {
-        std::cerr << "Error: " << e.what() << "\n";
+    catch (const exception& e) {
+        cerr << "Error: " << e.what() << "\n";
     }
 }
